b2d252c501f73fc60d5d06b5e8a4faa5e30a5020
Jyotsna Mehta <53934353+j24m@users.noreply.github.com>
2023-06-02 18:17:23 +0530

    Feat: Added back button to display input modal

--- index.js
+++ index.js
@@ -2,16 +2,23 @@ const numberOfFloors = document.querySelector(".no-of-floors");
 const numberOfLifts = document.querySelector(".no-of-lifts");
 const liftFloorValueModal = document.querySelector(".lift-floor-value-modal");
 const submitBtn = document.querySelector(".btn-submit");
-let showToastDiv = document.querySelector(".showToastDiv");
-let closeToastBtn = document.querySelector(".close-Toast");
-let toastMessage = document.querySelector(".toast-message");
+const showToastDiv = document.querySelector(".showToastDiv");
+const closeToastBtn = document.querySelector(".close-Toast");
+const toastMessage = document.querySelector(".toast-message");
+const backBtn = document.querySelector(".back-btn");
 
 closeToastBtn.addEventListener("click", () => {
   showToastDiv.style.display = "none";
 });
 
-submitBtn.addEventListener("click", displayLiftsAndFloors);
+backBtn.addEventListener("click", displayInputModal);
+function displayInputModal() {
+  backBtn.style.display = "none";
+  liftFloorValueModal.style.display = "flex";
+}
 
+submitBtn.addEventListener("click", displayLiftsAndFloors);
+backBtn.style.display = "none";
 showToastDiv.style.display = "none";
 function displayLiftsAndFloors() {
   const floorsValue = numberOfFloors.value;
@@ -21,6 +28,7 @@ function displayLiftsAndFloors() {
     showToastDiv.style.display = "block";
   } else {
     liftFloorValueModal.style.display = "none";
+    backBtn.style.display = "block";
     console.log(floorsValue, liftsValue);
   }
 }
