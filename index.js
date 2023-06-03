023f043738263e001b2f65a09879c60300dbf2fd
Jyotsna Mehta <53934353+j24m@users.noreply.github.com>
2023-06-03 16:24:55 +0530

    Feat : Generated the number of floors as per user input

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
 const numberOfFloors = document.querySelector(".no-of-floors");
 const numberOfLifts = document.querySelector(".no-of-lifts");
-const liftFloorValueModal = document.querySelector(".lift-floor-value-modal");
+const liftFloorValueModal = document.querySelector(".userInputContainer");
 const submitBtn = document.querySelector(".btn-submit");
 const showToastDiv = document.querySelector(".showToastDiv");
 const closeToastBtn = document.querySelector(".close-Toast");
 const toastMessage = document.querySelector(".toast-message");
 const backBtn = document.querySelector(".back-btn");
+const floorsContainer = document.querySelector(".floors-container");
+const floorsAndLiftsContainer = document.querySelector(
+  ".floors-lifts-container"
+);
 
 closeToastBtn.addEventListener("click", () => {
   showToastDiv.style.display = "none";
@@ -14,21 +18,35 @@ closeToastBtn.addEventListener("click", () => {
 backBtn.addEventListener("click", displayInputModal);
 function displayInputModal() {
   backBtn.style.display = "none";
+  floorsAndLiftsContainer.style.display = "none";
   liftFloorValueModal.style.display = "flex";
 }
 
-submitBtn.addEventListener("click", displayLiftsAndFloors);
+floorsAndLiftsContainer.style.display = "none";
 backBtn.style.display = "none";
 showToastDiv.style.display = "none";
+
+submitBtn.addEventListener("click", displayLiftsAndFloors);
+
 function displayLiftsAndFloors() {
   const floorsValue = numberOfFloors.value;
   const liftsValue = numberOfLifts.value;
-  if (floorsValue === "" || liftsValue === "") {
-    toastMessage.innerHTML = "Please add number of Floors and Lifts";
+  if (floorsValue <= 0 && liftsValue <= 0) {
+    toastMessage.innerHTML = "Please add valid number of Floors and Lifts";
     showToastDiv.style.display = "block";
   } else {
     liftFloorValueModal.style.display = "none";
     backBtn.style.display = "block";
     console.log(floorsValue, liftsValue);
+    floorsAndLiftsContainer.style.display = "block";
+    floorsContainer.innerHTML = "";
+
+    for (let i = floorsValue; i > 0; i--) {
+      floorsContainer.innerHTML;
+      const floorDiv = document.createElement("div");
+      floorDiv.classList.add("single-floor");
+      floorDiv.innerHTML = `<h1>Floor ${i}</h1>`;
+      floorsContainer.appendChild(floorDiv);
+    }
   }
 }
